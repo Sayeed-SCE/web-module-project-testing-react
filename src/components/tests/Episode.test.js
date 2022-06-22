fb36753b3594a969db1b8a216424c70301bb2b67
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-06-22 15:18:47 -0700

    completed Episode.test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,59 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 1,
+    name: "",
+    image: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
+    season: 1,
+    number: 1,
+    summary: 'test summary',
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", () => { });
+const testEpisodeWithoutImage = {
+    id: 1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: 'test summary',
+    runtime: 1
 
-test("renders default image when image is not defined", () => { });
+}
+
+test("renders without error", () => {
+    render (<Episode episode={testEpisode} />)
+ });
+
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testEpisode}/>);
+
+    const summary = screen.queryByText(/test summary/i)
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent("test summary");
+
+});
+
+
+test("renders default image when image is not defined", () => {
+    render (<Episode episode={testEpisodeWithoutImage} />);
+
+    const image =screen.queryByAltText('./stranger_things.png');
+    expect(image).toBeInTheDocument();
+    
+
+    // expect(image).toBeInTheDocument();
+ });
+
+
+// // * [ ] Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+// // * [ ] Modify the test data to display a specific summary statement. 
+// // Complete a test that shows that the summary value passed in to the Episode component displays as expected. 
+// // **Use at least then 3 different types of expect statements to test the the existence of the summary value.**
+// // * [ ] The episode component displays a default value ('https://i.ibb.co/2FsfXqM/stranger-things.png') 
+// when a image url is not provided. 
+// // Create a new piece of test data with the image property set to `null`. 
+// // Test that the alt tag of the image displayed is set to './stranger_things.png'.
