9d2464a8ab6c6aeb52553acac48296a55bd2a67f
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-06-23 15:34:40 -0700

    start of the Display.test challenge

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+const testShow ={
+    name: "test show",
+    summary: "test summary",
+    seasons: [
+        {
+            id: 0,
+            name: "season 1 ",
+            episodes: [ ]
+        },
+        {
+            id: 1,
+            name: "season 2 ",
+            episodes: [ ]
+        }
+    ]
 
-test('renders Show component when the button is clicked ', () => { });
+}
+
+
+test('renders without errors with no props', async () => { 
+    render(<Display />);
+});
+
+test('renders Show component when the button is clicked ', () => { 
+
+});
 
 test('renders show season options matching your data when the button is clicked', () => { });
